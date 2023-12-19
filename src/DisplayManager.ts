cb83496304b8d203d36c41ca1daf7ddd44721ac1
Blaž Ermenc <blaz.ermenc@student.um.si>
2023-12-19 19:03:13 +0100

    Premaknil loadOBJ

--- src/DisplayManager.ts
+++ src/DisplayManager.ts
@@ -119,54 +119,7 @@ export class DisplayManager{
         this._features.push(feature);
     }
 
-    loadOBJ(path: string, pos: T.Vector3, rot: T.Vector3, parent: T.Object3D = this._scene, objArray: T.Object3D[], callback?: EventListenerBinding[]): void {
-        let lastDot = path.lastIndexOf('.');
-        if (lastDot > 0) path = path.substring(0, lastDot);
 
-        const mtlPath = path + ".mtl";
-        const objPath = path + ".obj";
-
-        new MTLLoader()
-            .load(mtlPath,
-                (materials) => {
-                    materials.preload();
-                    new OBJLoader()
-                        .setMaterials(materials)
-                        .load(objPath,
-                            (object) => {
-                                object.traverse((child) => {
-                                    if(child instanceof T.Mesh){
-                                        console.log("Loaded mesh: " + child.name);
-                                        child.receiveShadow = true;
-                                        child.castShadow = true;
-                                        if(callback != undefined) {
-                                            for (const binding of callback) {
-                                                child.addEventListener(binding.type, binding.listener);
-                                            }
-                                        }
-                                    }
-                                });
-                                object.position.copy(pos);
-                                object.rotateOnWorldAxis(new T.Vector3(1, 0, 0), rot.x);
-                                object.rotateOnWorldAxis(new T.Vector3(0, 1, 0), rot.y);
-                                object.rotateOnWorldAxis(new T.Vector3(0, 0, 1), rot.z);
-                                object.receiveShadow = true;
-                                object.castShadow = true;
-                                object.updateMatrix();
-
-                                parent.add(object);
-                                objArray.push(object);
-                            },
-                            function ( xhr ) {
-                                console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-                            },
-                            // called when loading has errors
-                            function ( error ) {
-                                console.log( 'An error happened: ' + error );
-                            }
-                        );
-                })
-    }
 
     onMouseInteractClick(event: MouseEvent){
         if (!this.cam) {
